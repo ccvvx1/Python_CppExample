7b796adfe632e24f9137822a5d5bda2deee643ad
tanmingwu <13802885497@139.com>
2024-10-14 16:54:41 +0800

    feat: 1 ok

--- freqencoder/src/bindings.cpp
+++ freqencoder/src/bindings.cpp
@@ -5,4 +5,5 @@
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     m.def("freq_encode_forward", &freq_encode_forward, "freq encode forward (CUDA)");
     m.def("freq_encode_backward", &freq_encode_backward, "freq encode backward (CUDA)");
+    m.def("freq_print", &freq_print, "freq print");
 }
--- freqencoder/src/freqencoder.h
+++ freqencoder/src/freqencoder.h
@@ -8,3 +8,5 @@ void freq_encode_forward(at::Tensor inputs, const uint32_t B, const uint32_t D, 
 
 // _backend.freq_encode_backward(grad, outputs, B, input_dim, degree, output_dim, grad_inputs)
 void freq_encode_backward(at::Tensor grad, at::Tensor outputs, const uint32_t B, const uint32_t D, const uint32_t deg, const uint32_t C, at::Tensor grad_inputs);
+
+void freq_print();
